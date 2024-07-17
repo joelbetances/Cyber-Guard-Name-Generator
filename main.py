5e796539fd97b73e5b55679753560fcf06b90b6a
Joel Betances <30453578+joelbetances@users.noreply.github.com>
2024-07-17 17:28:41 -0400

    Cyber Guard Name Generator

---
+++ main.py
@@ -0,0 +1,15 @@
+#1. Create a greeting for your program.
+print("Welcome to the Cyber Guard Name Generator. Generates strong and secure-sounding names for cybersecurity firms")
+
+#2. Ask the user for the last city that they visited.
+street = input("What's the name of the city you last visited?\n")
+
+#3. Ask the user for a there favorite color.
+color = input("What's your favorite color?\n")
+
+#4. Ask the user for a number.
+number = input("What's your favorite number?\n")
+
+#5. Combine the name of the city, color, and number to show them their Cyber Guard name.
+print("Your Cyber Guard name could be " + street + " " + color + " " + number)
+#6. Make sure the input cursor shows on a new line:
